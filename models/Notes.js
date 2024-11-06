d00516247c22418e66d723e08fe72be861c1bbcf
Mithil Suthar <136597187+Mithil2603@users.noreply.github.com>
2024-11-06 15:19:19 +0530

    topicpoints updated

--- models/Notes.js
+++ models/Notes.js
@@ -12,9 +12,12 @@ const notesSchema = new mongoose.Schema({
     topicDescription: {
         type: String,
     },
-    topicPoints: {
-        type: [String],
-    }, 
+    topicPoints: [
+        {
+            type: mongoose.Schema.Types.Mixed, // Allows both strings and objects
+            default: []
+        }
+    ],
     topicImages: {
         type: String,
     }
